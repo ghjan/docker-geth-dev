6bc01252c046304c2cc8b3399e6c197f858846fd
jian zhang <jian.zhang@fuwo.com>
2018-01-11 13:24:04 +0800

    remove cd /root

--- init-private-net.sh
+++ init-private-net.sh
@@ -1,2 +1,1 @@
-cd /root
 geth --identity "testchain_mainnode" --rpc --rpccorsdomain "*"  --rpcapi "db,eth,net,web3,admin,miner,personal,rpc" --networkid 66666  init Testchaini_david.json
--- start-private-main.sh
+++ start-private-main.sh
@@ -1,2 +1,1 @@
-cd /root
 geth --identity "testchain_mainnode"  --rpc  --rpccorsdomain "*" --port "30303"  --rpcapi "db,eth,net,web3,admin,miner,personal,rpc"  --networkid 66666 console 2>1.txt
